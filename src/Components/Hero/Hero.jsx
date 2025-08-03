4ef953df5252dc273c71ca2a65fcd3a905f7abf5
Tejas srivastava <tejassrivast@gmail.com>
2025-08-03 22:14:50 +0530

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,17 +6,17 @@ import About from '../About'
 const Hero = ({ theme, settheme }) => {
   return (
     <>
-      <div className="w-full h-screen flex justify-center items-center text-xl">
+      <div className="w-full h-screen  flex justify-center items-center text-xl">
         <div className="flex flex-col sm:flex-row justify-between items-center w-full sm:w-1/2 mx-auto px-4 py-3 uppercase gap-4">
-          <div className="left flex-1 flex sm:flex-col flex-row normal-case  text-center">
-            <img  className='h-1/2 w-1/2' src={codeguy} alt="" />
-            <div className='flex  flex-col sm:ml-1 mt-8 ml-8'>
-              <button className="bg-gradient-to-r sm:w-1/2 w-full mb-2 from-indigo-500 to-purple-500 text-white font-semibold py-2 px-6 rounded-2xl hover:scale-105 transition duration-300">
+          <div className="left flex-1 flex  items-center sm:flex-col flex-row normal-case  text-center">
+            <img  className='h-1/2 w-1/2  mb-2.5  lg:h-3/4 lg:w-3/4' src={codeguy} alt="" />
+            <div className='flex  flex-col sm:ml-1  ml-8'>
+              <button className="bg-gradient-to-r  from-indigo-500 to-purple-500 text-white font-semibold sm:w-1/2 w-full mb-2 py-2 px-6 rounded-2xl hover:scale-105 transition duration-300">
                  <a href="https://drive.google.com/file/d/1HmguiPTm92pGUIKqS0S_kikydgEkzBVY/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                     Resume
                    </a>
              </button>
-             <button className="bg-gradient-to-r sm:w-1/2 w-full from-indigo-500 to-purple-500 text-white font-semibold py-2 px-6 rounded-2xl hover:scale-105 transition duration-300"><Modal/></button>
+             <button className="bg-gradient-to-r sm:w-1/2 w-full sm:mb-30 from-indigo-500 to-purple-500 text-white font-semibold py-2 px-6 rounded-2xl hover:scale-105 transition duration-300"><Modal/></button>
                
 
             </div>
