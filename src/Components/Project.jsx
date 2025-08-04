2e473c3d89e49721c7080f99c0b6af576225f0f9
Tejas srivastava <tejassrivast@gmail.com>
2025-08-04 18:35:37 +0530

    some work done

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import React from 'react';
+import coding from '../assets/coding-pov.png'
+import mern from '../assets/mern.avif'
 
 const Project = () => {
   return (
     <>
-    <div className="w-full h-screen ">
+      <h2 className="text-3xl font-bold text-center  m-10 ">
+        Technologies I Work With:
+      </h2>
 
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur quae molestiae cum sint error. Ab, laudantium architecto iusto ratione ipsa facilis debitis nesciunt numquam, consequatur sequi a consequuntur aut distinctio!
-    </div>
-    
+      <div className="w-full  flex items-center  justify-center px-4 pt-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-5xl w-full">
+          <div className="bg-gradient-to-r from-[#1f1a33] to-[#363046]  rounded-xl shadow-md md: row-span-2">
+           <img src={coding} alt="Coding" className="w-full h-full object-cover -z-10  pb-6 
+           " />
+           
+
+         </div>
+          <div className="bg-gradient-to-r from-[#1f1a33] to-[#363046] p-8 rounded-xl shadow-lg space-y-4">
+  <h2 className="text-2xl font-extrabold text-center text-amber-500">
+    Code Is Craft
+  </h2>
+  <p className="text-white text-center leading-relaxed">
+    Every line of code is a chance to innovate, solve problems, and make a difference.
+  </p>
+</div>
+
+          <div className="bg-white p-0 rounded-xl shadow-md"> <img src={mern} alt="Coding" className="w-full h-full object-cover rounded-xl 
+           " /></div>
+         
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
