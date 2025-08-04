c862abc7bd2157fcbffe9660991497300264a05e
Tejas srivastava <tejassrivast@gmail.com>
2025-08-04 19:11:51 +0530

    trying

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import coding from '../assets/coding-pov.png'
 import mern from '../assets/mern.avif'
-
+import codeguy from '../assets/coding_guy-removebg-preview.png'
+import fle from '../assets/fle.svg'
 const Project = () => {
   return (
     <>
@@ -11,19 +12,30 @@ const Project = () => {
 
       <div className="w-full  flex items-center  justify-center px-4 pt-10">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-5xl w-full">
-          <div className="bg-gradient-to-r from-[#1f1a33] to-[#363046]  rounded-xl shadow-md md: row-span-2">
-           <img src={coding} alt="Coding" className="w-full h-full object-cover -z-10  pb-6 
-           " />
-           
+          <div className="bg-gradient-to-r  from-[#1f1a33] to-[#363046] overflow-hidden relative rounded-xl shadow-md md:row-span-2">
+         <div className="relative z-10 flex flex-col items-center justify-center h-full  bg-opacity-40 px-4 text-center">
+    <h2 className="text-3xl font-extrabold text-amber-400 drop-shadow-md">
+      Code Is Craft
+    </h2>
+    <p className="text-white mt-2 leading-relaxed drop-shadow-sm max-w-md">
+      Every line of code is a chance to innovate, solve problems, and make a difference.
+    </p>
+  </div>
+          <img src={coding} className='w-full h-full absolute object-cover top-7' alt="" />
 
          </div>
-          <div className="bg-gradient-to-r from-[#1f1a33] to-[#363046] p-8 rounded-xl shadow-lg space-y-4">
-  <h2 className="text-2xl font-extrabold text-center text-amber-500">
-    Code Is Craft
-  </h2>
-  <p className="text-white text-center leading-relaxed">
-    Every line of code is a chance to innovate, solve problems, and make a difference.
-  </p>
+          <div className="bg-gradient-to-r from-[#1f1a33] to-[#363046]  flex flex-wrap rounded-xl shadow-lg space-y-4">
+     <img src={fle} alt="" className='h-14 mt-2 ml-2 cursor-pointer' />
+          <img src={fle} alt="" className='h-14 mt-2 ml-2 cursor-pointer' />
+          <img src={fle} alt="" className='h-14 mt-2 ml-2 cursor-pointer' />
+          <img src={fle} alt="" className='h-14 mt-2 ml-2 cursor-pointer' />
+          <img src={fle} alt="" className='h-14 mt-2 ml-2 cursor-pointer' />
+          <img src={fle} alt="" className='h-14 mt-2 ml-2 cursor-pointer' />
+          <img src={fle} alt="" className='h-14 mt-2 ml-2 cursor-pointer' />
+          <img src={fle} alt="" className='h-14 mt-2 ml-2 cursor-pointer' />
+          <img src={fle} alt="" className='h-14 mt-2 ml-2 cursor-pointer' />
+          <img src={fle} alt="" className='h-14 mt-2 ml-2 cursor-pointer' />
+          <img src={fle} alt="" className='h-14 mt-2 ml-2 cursor-pointer' />
 </div>
 
           <div className="bg-white p-0 rounded-xl shadow-md"> <img src={mern} alt="Coding" className="w-full h-full object-cover rounded-xl 
